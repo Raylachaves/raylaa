e593fd837e5cee486597f442e1827188c5e11035
Raylachaves <96721847+Raylachaves@users.noreply.github.com>
2021-12-28 04:00:09 -0300

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,5 @@
 key='dfa04c12-c7ff-4d8b-b87d-76c4234edcba'
-app_name="raylaa"
+app_name="Gilbert"
 deviceid="32976A152D1E2D2E2EAD36C257352959B3D7A603DD7F60A8F5F573FD1C0E865CF10D576CC03AEB826F"
 emaill="oeddzztalu@vddaz.com"
 passwordd="91793632"
