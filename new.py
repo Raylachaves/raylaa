e033d08fe57b907dc7af8be12e1f4c5511f3c30c
Raylachaves <96721847+Raylachaves@users.noreply.github.com>
2022-01-01 02:02:10 -0300

    Update new.py

--- new.py
+++ new.py
@@ -1,10 +1,10 @@
 key='dfa04c12-c7ff-4d8b-b87d-76c4234edcba'
 app_name="raylaa"
-deviceid="32976A152D1E2D2E2EAD36C257352959B3D7A603DD7F60A8F5F573FD1C0E865CF10D576CC03AEB826F"
-emaill="oeddzztalu@vddaz.com"
+deviceid="320389C184236BD69DACC1B736FC50695EA2441168F132E6A26A3F25DE0FF0A20C9841A650E5145C1E"
+emaill="knkx7qbs3st@wwjmp.com"
 passwordd="91793632"
 custompwd="91793632"
 private=""
 chatlink="http://aminoapps.com/p/ox4h0b"
-nickname="Biscoitinho"
+nickname="Fent"
 replit="https://SeashellMediumblueRegister.raylachaves.repl.co"
